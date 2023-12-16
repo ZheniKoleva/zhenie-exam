09d476c4dbfd06f46c14cc45638f287a720d5a40
ZheniKoleva <zhenie_koleva@abv.bg>
2023-12-16 14:07:58 +0200

    fixed About page tests

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -4,12 +4,14 @@ test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
-  expect(text).toBe('About my shopping list');
+  const expected = 'About';
+  expect(text).toBe(expected);
 });
 
 test('Check about page test', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
   const heading = await page.$('p');
   const text = await heading.textContent();
-  expect(text).toBe('This is my shopping list');
+  const expected = 'This is the Regular exam for Software Engineering and DevOps course @ SoftUni'
+  expect(text).toBe(expected);
 });
