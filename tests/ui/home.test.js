4d22663b1dc2a76b6bb9a5c659c6d9561932124f
ZheniKoleva <zhenie_koleva@abv.bg>
2023-12-16 14:20:29 +0200

    fixed Check home page test

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -2,8 +2,9 @@ const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
     await page.goto('http://localhost:8080/');
-    const heading = await page.$('h2');
+    const heading = await page.$('h1');
     const text = await heading.textContent();
-    expect(text).toContain('My shopping list');  
+    const expected = 'My Shopping List';
+    expect(text).toContain(expected);  
   });
   
